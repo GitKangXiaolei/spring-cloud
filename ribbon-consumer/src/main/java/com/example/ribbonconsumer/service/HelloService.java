f22688fafb2003052a9877c24b615bdb27085cab
GitKangXiaolei <MicroKangXiaolei@outlook.com>
2018-10-16 17:01:44 +0900

    Initial commit

---
+++ ribbon-consumer/src/main/java/com/example/ribbonconsumer/service/HelloService.java
@@ -0,0 +1,25 @@
+package com.example.ribbonconsumer.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@EnableHystrix
+public class HelloService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "helloFallback")
+    public String helloservice() {
+        return restTemplate
+                .getForEntity("http://HELLO-SERVICE", String.class)
+                .getBody();
+    }
+
+    public String helloFallback() {
+        return "error";
+    }
+}
